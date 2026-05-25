c675694f27e0eaaa7ee444a562604fc8060613cb
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2026-05-26 01:18:46 +0200

    TextType enum and TextNode class added. This will represent inline text elements, stuff that we'll inject into blocks later eventually.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextType
+from textnode import TextNode
+
+
+def main() -> None:
+    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+
+
+if __name__ == "__main__":
+    main()
