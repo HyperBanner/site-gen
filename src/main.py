4121712ae16172eb2fe1623ce80860b72d983232
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2026-06-06 07:09:01 +0200

    Added funcs to main.py to recursively copy the contents of static/ to public/. Also added public/ to .gitignore

--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
-from textnode import TextType
-from textnode import TextNode
+import os
+import shutil
+from pathlib import Path
 
 
 def main() -> None:
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    copy_static_to_public()
+
+
+def copy_static_to_public() -> None:
+    source_dir = Path(__file__).resolve().parent.parent / "static"
+    dest_dir = Path(__file__).resolve().parent.parent / "public"
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+    print("Destination directory cleaned")
+    copy_tree(source_dir, dest_dir)
+
+
+# helper func for copy_static_to_public
+def copy_tree(source_dir, dest_dir) -> None:
+    items = os.listdir(source_dir)
+    for item in items:
+        item_path = os.path.join(source_dir, item)
+        dest_item_path = os.path.join(dest_dir, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dest_dir)
+            print(f"Copied file to path: '{dest_item_path}' from: '{item_path}'")
+        elif os.path.isdir(item_path):
+            os.mkdir(dest_item_path)
+            print(f"Made directory to path: '{dest_item_path}' from: '{item_path}'")
+            copy_tree(item_path, dest_item_path)
 
 
 if __name__ == "__main__":
